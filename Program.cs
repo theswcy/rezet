20d0b9b0e545395346a0d3525aee606011892869
Horizon Witwicky <theswcy@gmail.com>
2024-10-26 00:35:57 -0300

    10 to 1.

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Rezet
         public static DiscordClient? Rezet;
         public static CommandsNextExtension? commands;
         public static DatabaseService? _databaseService;
-        private static readonly TimeSpan StatusUpdateInterval = TimeSpan.FromMinutes(10);
+        private static readonly TimeSpan StatusUpdateInterval = TimeSpan.FromMinutes(1);
         static async Task Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.White;
