650fdc135aabbafe516c94c81bd9ab2ebee2a4eb
Horizon Witwicky <theswcy@gmail.com>
2024-11-10 16:29:39 -0300

    Novas variáveis da função de parcerias funcionando!

--- bot/commands/partnership/functions/SwitchStringsAndMore.cs
+++ bot/commands/partnership/functions/SwitchStringsAndMore.cs
@@ -19,22 +19,28 @@ public static class SwitchOrResponse
                         .Replace("@[guild.id]", ctx.Guild.Id.ToString())
 
                         // ========== NEW:
-                        .Replace("@[partner.name]", "Name of the partner guild.")
-                        .Replace("@[rep.name]", "Name of the partner rep.")
-                        .Replace("@[rep.id]", "Id of the rep.")
-                        .Replace("@[rep.mention]", "Mention of the rep.");
+                        .Replace("@[partner.name]", "Example Community")
+                        .Replace("@[rep.name]", "Rep Name")
+                        .Replace("@[rep.id]", "1234567890.")
+                        .Replace("@[rep.mention]", "@rep.");
         return txt2;
     }
-    public static string SwitchVariables2(string txt, MessageCreateEventArgs ctx, int? points = null)
+    public static string SwitchVariables2(string txt, MessageCreateEventArgs ctx, DiscordUser rep, int? points = null, int? ranking = null, string? pguild = null)
     {
         var txt2 = txt.Replace("@[staff.mention]", ctx.Author.Mention)
                         .Replace("@[staff.name]", ctx.Author.Username)
                         .Replace("@[staff.id]", ctx.Author.Id.ToString())
                         .Replace("@[staff.points]", $"{points ?? 10}")
+                        .Replace("@[staff.rank]", $"{ranking ?? 99}º")
 
                         .Replace("@[guild.name]", ctx.Guild.Name)
                         .Replace("@[guild.owner]", ctx.Guild.Owner.DisplayName)
-                        .Replace("@[guild.id]", ctx.Guild.Id.ToString());
+                        .Replace("@[guild.id]", ctx.Guild.Id.ToString())
+
+                        .Replace("@[partner.name]", $"{pguild ?? "Example Community"}")
+                        .Replace("@[rep.name]", $"{rep.Username}")
+                        .Replace("@[rep.id]", $"{rep.Id}")
+                        .Replace("@[rep.mention]", $"{rep.Mention}");
         return txt2;
     }
 }
