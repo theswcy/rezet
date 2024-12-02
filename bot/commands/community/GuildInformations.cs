2bc5ab824dc2adf6f6239abf49a1a87858af2b03
Horizon Witwicky <theswcy@gmail.com>
2024-12-02 16:33:21 -0300

    Novo comando: @rezet guild stats.

--- bot/commands/community/GuildInformations.cs
+++ bot/commands/community/GuildInformations.cs
@@ -3,10 +3,77 @@ using DSharpPlus.SlashCommands;
 using DSharpPlus.Entities;
 using Rezet;
 using RezetSharp;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
 
 
 
 #pragma warning disable CS8602
+public class CommunityCommands_prefix : BaseCommandModule
+{
+    [Command("servidor")]
+    [Aliases("server", "sv", "guild", "g", "community")]
+    public async Task GuildCommands(CommandContext ctx, string CMD)
+    {
+        try
+        {
+            if (CMD == "stats")
+            {
+                var Members = await ctx.Guild.GetAllMembersAsync();
+                double onlineMembers = Members.Count(m => m.Presence != null && 
+                (m.Presence.Status == UserStatus.Online || 
+                 m.Presence.Status == UserStatus.Idle || 
+                 m.Presence.Status == UserStatus.DoNotDisturb));
+
+                var embed = new DiscordEmbedBuilder()
+                {
+                    Description = "## <:rezet_plant:1308125160577962004> Guild Stats!",
+                    Color = new DiscordColor("#7e67ff")
+                };
+                int OnMe = (int)Math.Round(onlineMembers / Members.Count * 100); 
+                int OfMe = (int)Math.Round((Members.Count - onlineMembers) / Members.Count  * 100);
+                embed.AddField(
+                    "<:rezet_settings1:1147163366561955932> Porcentagem:",
+                    $"> <:rezet_globo:1147178426927681646> **Total**: `{Members.Count}`" +
+                    $"\n> <:rezet_dgreen:1147164307889586238> **Online**: `{onlineMembers}` ╺╸ **{OnMe}%**" +
+                    $"\n> <:rezet_dred:1147164215837208686> **Offline**: `{Members.Count - onlineMembers}` ╺╸ **{OfMe}%**"
+                );
+                double per = onlineMembers / Members.Count * 100;
+                int GraphCount = (int)Math.Round(per / 10);
+                string ONM = "<:rezet_dgreen:1147164307889586238>";
+                string OFM = "<:rezet_dred:1147164215837208686>";
+                string Graph = "";
+                for (int i = 0; i < 10; i++)
+                {
+                    if (i < GraphCount) {Graph += ONM; }
+                    else { Graph += OFM; }
+                }
+                embed.AddField(
+                    "<:rezet_channels:1308125117875752961> Gráfico:",
+                    $"\n> {Graph}"
+                );
+
+
+
+                await ctx.RespondAsync(
+                    builder: new DiscordMessageBuilder().WithContent("Bip bup bip").AddEmbed(embed)
+                );
+            }
+        }
+        catch (Exception ex)
+        {
+            await ctx.RespondAsync(
+                builder: new DiscordMessageBuilder()
+                    .WithContent($"Falha ao executar o comando.\n\n> `{ex.Message}`"));
+        }
+    }
+}
+
+
+
+
+
+
 [SlashCommandGroup("community", "Community commands")]
 public class CommunityCommands : ApplicationCommandModule
 {
@@ -70,11 +137,14 @@ public class CommunityCommands : ApplicationCommandModule
             
             embed.AddField(
                 "<:rezet_connect:1147907330378309652> Channels",
-                $"> **Text**: {Channels.Count(c => c.Type == ChannelType.Text)}" + 
-                $"\n> **Voice** {Channels.Count(c => c.Type == ChannelType.Voice)}" +
-                $"\n> **Forum**: {Channels.Count(c => c.Type == ChannelType.GuildForum)}" +
-                $"\n> **Announcement**: {Channels.Count(c => c.Type == ChannelType.News)}" +
-                $"\n> **Category**: {Channels.Count(c => c.Type == ChannelType.Category)}"
+                $"> **Text**: `{Channels.Count(c => c.Type == ChannelType.Text)}`" + 
+                $"\n> **Voice** `{Channels.Count(c => c.Type == ChannelType.Voice)}`" +
+                $"\n> **Forum**: `{Channels.Count(c => c.Type == ChannelType.GuildForum)}`" +
+                $"\n> **Stage**: `{Channels.Count(c => c.Type == ChannelType.Stage)}`" +
+                $"\n> **Category**: `{Channels.Count(c => c.Type == ChannelType.Category)}`" +
+                $"\n> **Public Threads**: `{Channels.Count(c => c.Type == ChannelType.PublicThread)}`" +
+                $"\n> **Private Threads**: `{Channels.Count(c => c.Type == ChannelType.PrivateThread)}`" +
+                $"\n> **Announcement**: `{Channels.Count(c => c.Type == ChannelType.News)}`" 
             );
 
 
